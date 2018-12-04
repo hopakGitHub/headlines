94880476efaed9502b8ecfe130c58c4799cd1943
Jung Ho Park <park.nano@gmail.com>
2018-12-04 18:25:15 +0000

    initial commit

---
+++ headlines.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("/usr/local/lib/python2.7/dist-packages")
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+        return "Hello, World!"
+
+if __name__ == '__main__':
+        app.run()
